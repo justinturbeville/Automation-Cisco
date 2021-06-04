be079524d843476ce0b3a938f69441ffe15412a0
justinturbeville <59580400+justinturbeville@users.noreply.github.com>
2021-06-04 15:49:33 -0400

    Update ansible_setup.sh

--- ansible_setup.sh
+++ ansible_setup.sh
@@ -3,5 +3,9 @@
 sudo apt installl ansible -y
 ansible-galaxy collection install cisco.ios
 ansible-galaxy collection install cisco.iosxr
+ansible-galaxy collection install cisco.asa
+ansible-galaxy collection install cisco.meraki
+ansible-galaxy collection install cisco.ucs
+ansible-galaxy collection install cisco.nxos
 sudo apt instal python3-pip -y
 pip install paramiko

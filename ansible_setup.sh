9660955f75e49a70cec7e1625aeaf71d623a6b66
justinturbeville <59580400+justinturbeville@users.noreply.github.com>
2021-06-04 15:52:52 -0400

    Update ansible_setup.sh

--- ansible_setup.sh
+++ ansible_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This will update your system. You can choose not if you do not want. 
+# You need to atleast update the repository list if you have not done so already.
+# If you do choose to update the system fully make sure that you reboot the system after updates are complete.
+sudo apt update && sudo apt upgrade -y
+
+# This will install ansible and the aproriate dependencies such as paramiko and some of the Cisco ansible modules.
 sudo apt installl ansible -y
 ansible-galaxy collection install cisco.ios
 ansible-galaxy collection install cisco.iosxr
